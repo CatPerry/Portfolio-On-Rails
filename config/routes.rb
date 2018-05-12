e9119615b4ed11eb989d5925dff46e22b30faeba
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-05-12 13:07:39 -0400

    Created portfolio Model via resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
   get 'pages/about'
-  get 'pages/portfolio'
+  # get 'pages/portfolio'
   get 'pages/contact'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
